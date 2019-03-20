e6e1933d1058e9447a2ef0e5efa0875ae512f597
sunshine2329 <seniordeveloper00001@gmail.com>
2019-03-20 08:06:10 +0000

    Creating Mask

--- DepthImage/Utilites/DepthImageFilters.swift
+++ DepthImage/Utilites/DepthImageFilters.swift
@@ -26,7 +26,33 @@ class DepthImageFilters {
     }
     
     func createMask(for depthImage: CIImage, withFocus focus: CGFloat, andScale scale: CGFloat) -> CIImage {
-        return depthImage
+        let s1 = MaskParams.slope
+        let s2 = -MaskParams.slope
+        let filterWidth =  2 / MaskParams.slope + MaskParams.width
+        let b1 = -s1 * (focus - filterWidth / 2)
+        let b2 = -s2 * (focus + filterWidth / 2)
+        
+        let mask0 = depthImage
+            .applyingFilter("CIColorMatrix", parameters: [
+                "inputRVector": CIVector(x: s1, y: 0, z: 0, w: 0),
+                "inputGVector": CIVector(x: 0, y: s1, z: 0, w: 0),
+                "inputBVector": CIVector(x: 0, y: 0, z: s1, w: 0),
+                "inputBiasVector": CIVector(x: b1, y: b1, z: b1, w: 0)])
+            .applyingFilter("CIColorClamp")
+        
+        let mask1 = depthImage
+            .applyingFilter("CIColorMatrix", parameters: [
+                "inputRVector": CIVector(x: s2, y: 0, z: 0, w: 0),
+                "inputGVector": CIVector(x: 0, y: s2, z: 0, w: 0),
+                "inputBVector": CIVector(x: 0, y: 0, z: s2, w: 0),
+                "inputBiasVector": CIVector(x: b2, y: b2, z: b2, w: 0)])
+            .applyingFilter("CIColorClamp")
+
+        let combinedMask = mask0.applyingFilter("CIDarkenBlendMode", parameters: ["inputBackgroundImage" : mask1])
+        
+        let mask = combinedMask.applyingFilter("CIBicubicScaleTransform", parameters: ["inputScale": scale])
+        
+        return mask
     }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Image-Depth-Maps
+
 Image Depth Maps for iOS
+
+- Generates depth information.
+- Read depth data from images.
+- Combine this depth data with filters to create neat effects.
